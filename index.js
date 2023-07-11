dc5764f848e455221306408d9b4cc5e625733ccd
kielkow <matheuskiel@fiorifer.com.br>
2023-07-11 13:01:09 -0300

    migrate project

---
+++ index.js
@@ -0,0 +1,28 @@
+import 'dotenv/config'
+import { MongoClient } from 'mongodb'
+
+const mongoDBV4 = new MongoClient(process.env.ORIGIN_URL)
+const mongoDBV5 = new MongoClient(process.env.DESTINATION_URL)
+
+async function run() {
+	try {
+		const dbV4 = mongoDBV4.db(process.env.ORIGIN_DB)
+		const dbV5 = mongoDBV5.db(process.env.DESTINATION_DB)
+
+		const storesCollectionV4 = dbV4.collection(process.env.ORIGIN_COLLECTION)
+		const storesCollectionV5 = dbV5.collection(process.env.DESTINATION_COLLECTION)
+
+		const storesDataV4 = storesCollectionV4.find()
+
+		for await (const store of storesDataV4) {
+			const result = await storesCollectionV5.insertOne(store)
+			console.log(`A document was inserted with the _id: ${result.insertedId}`);
+		}
+	}
+	finally {
+		await mongoDBV4.close()
+		await mongoDBV5.close()
+	}
+}
+
+run().catch(console.dir)
